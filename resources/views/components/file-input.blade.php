b9abe6d5a65bf39806175c8b838567f2341def62
Rizki5846 <rizkiardiansyah584@gmail.com>
2024-01-04 13:47:27 +0700

    Pembuatan Component

---
+++ resources/views/components/file-input.blade.php
@@ -0,0 +1,8 @@
+<input type="file" {!! $attributes->merge(['class' => 'block w-full border border-white shadow-sm rounded-md text-sm
+    focus:z-10 focus:border-indigo-500 focus:ring-indigo-500
+    dark:focus:border-indigo-600 dark:focus:ring-indigo-600
+    dark:bg-white dark:border-gray-300 dark:text-gray-700
+    file:bg-transparent file:border-0
+    file:bg-gray-300 file:mr-4
+    file:py-3 file:px-4
+    dark:file:bg-gray-700 dark:file:text-gray-400']) !!}>
